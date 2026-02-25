9c74459cb75bcefee2e8334842e985b5698749ad
ChrisV3 <67931838+ChrisVul3@users.noreply.github.com>
2026-02-25 16:35:41 -0500

    frontend topbar, sidebar

---
+++ rocky-interface/src/lib/components/shell/Sidebar.svelte
@@ -0,0 +1,11 @@
+<nav class="nav">
+	<button type="button" class="nav-link">Account</button>
+	<button type="button" class="nav-link">Dashboard</button>
+	<button type="button" class="nav-link">Courses</button>
+	<button type="button" class="nav-link">Analytics</button>
+	<button type="button" class="nav-link">Users</button>
+
+	<div class"spacer"></div>
+
+	<button type="button" class="nav-link">Help</button>
+</nav>
---
+++ rocky-interface/src/lib/components/shell/Topbar.svelte
@@ -0,0 +1,9 @@
+<header class="topbar">
+	<div class="brand">
+		<img src="/rock.jpg" alt="Rocky" class="brand-logo" />
+		<div>
+			<div class="brand-name">Rocky</div>
+			<div class="brand-sub">Administrator</div>
+		</div>
+	</div>
+</header>
