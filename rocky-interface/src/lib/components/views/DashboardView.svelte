3d7697261e4d52569c40c21dc343d0e14b8f0181
ChrisV3 <67931838+ChrisVul3@users.noreply.github.com>
2026-03-02 12:21:48 -0500

    Fixed PR: | added h1 for visuals

---
+++ rocky-interface/src/lib/components/views/DashboardView.svelte
@@ -0,0 +1,1 @@
+<h1>DashboardView</h1>
--- rocky-interface/src/routes/app/+page.svelte
+++ rocky-interface/src/routes/app/+page.svelte
@@ -1,5 +1,5 @@
-<script>
-	import { currentFrame } from '$lib/stores/frameStore';
+<script lang="ts">
+	import { currentFrame, type Frame } from '$lib/stores/frameStore';
 
 	import DashboardView from '$lib/components/views/DashboardView.svelte';
 	import UsersView from '$lib/components/views/UsersView.svelte';
@@ -7,18 +7,15 @@
 	import AnalyticsView from '$lib/components/views/AnalyticsView.svelte';
 	import AccountView from '$lib/components/views/AccountView.svelte';
 	import HelpView from '$lib/components/views/HelpView.svelte';
+
+	const frames: Record<Frame, typeof DashbosrdView> = {
+	    dashboard: DashboardView,
+	    users: UsersView,
+	    courses: CoursesView,
+	    analytics: AnalyticsView,
+	    account: AccountView,
+	    help: HelpView
+	  };
 </script>
 
-{#if $currentFrame === 'dashboard'}
-	<DashboardView />
-{:else if $currentFrame === 'users'}
-	<UsersView />
-{:else if $currentFrame === 'courses'}
-	<CoursesView />
-{:else if $currentFrame === 'analytics'}
-	<AnalyticsView />
-{:else if $currentFrame === 'account'}
-	<AccountView />
-{:else if $currentFrame === 'help'}
-	<HelpView />
-{/if}
+<svelte:component this={frames[$currentFrame]} />
